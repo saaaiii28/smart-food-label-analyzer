41caceba03c071290487f5abb10f05013694f8a3
Nandhana242005 <nandhanap123@gmail.com>
2026-03-27 07:36:27 +0530

    frontpush

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from ingredient import *
 from model import load_model
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.secret_key = "supersecretkey"
